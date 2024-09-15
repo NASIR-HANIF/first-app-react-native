0dc59f586230d25136a6bc7c6741aae55aaf6b97
nasir hanif <nasir127sb@gmail.com>
2024-09-15 19:55:34 +0500

    h

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { View ,Text} from "react-native"
+const App = ()=>{
+ const design =(
+    <>
+      <View style={{backgroundColor : "green",flex : 1}}>
+        <Text>abdulhadi nasir</Text>
+      </View>
+    </>
+ );
+ return design
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
